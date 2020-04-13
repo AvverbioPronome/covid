cbd6dcabc7457a8ef1db414b56c55ab7f12eefaa
Giuseppe C <AvverbioPronome@users.noreply.github.com>
2020-04-13 11:12:43 +0200

    try new script

--- pull.sh
+++ pull.sh
@@ -1,3 +1,24 @@
+#!/bin/sh
+
+if [ ! -z "$(git status --porcelain)" ]; then
+    git stash push
+    STASHED="true"
+fi
+
 git pull
 git submodule update --init --remote
-git add data && git commit -m "$(date +%F)"
+
+if [ ! -z "$(git status --porcelain)" ]; then
+    git add data && git commit -m "$(date +%F): pull pcm-dpc data"
+
+    docker run -v "$(pwd):/home/jovyan/covid" jupyter/scipy-notebook \
+           jupyter nbconvert --inplace --execute '/home/jovyan/covid/*.ipynb'
+
+    git add '*.ipynb' && git commit -m "Re-run with $(git log -1 --format=%s data|cut -d':' -f 1) data."
+fi
+
+if [ ! -z "$STASHED" ]; then
+    git stash pop --quiet
+    STASHED=""
+fi
+
