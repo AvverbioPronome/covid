e7cc4da9ed555ddcdb7c14c067262cd91af2cba3
Giuseppe C <AvverbioPronome@users.noreply.github.com>
2020-04-13 13:15:57 +0200

    more sensible commit messages

--- pull.sh
+++ pull.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+cd "$(dirname $0)"
+
 if [ ! -z "$(git status --porcelain)" ]; then
     git stash push
     STASHED="true"
@@ -8,13 +10,13 @@ fi
 git pull
 git submodule update --init --remote
 
-if [ ! -z "$(git status --porcelain)" ]; then
-    git add data && git commit -m "$(date +%F): pull pcm-dpc data"
+if [ ! -z "$(git status --porcelain)" ] || [ "$1" = "--force" ]; then
+    git add data && git commit -m "$(date +%F): pull pcm-dpc/COVID-19"
 
     docker run -v "$(pwd):/home/jovyan/covid" jupyter/scipy-notebook \
            jupyter nbconvert --inplace --execute '/home/jovyan/covid/*.ipynb'
 
-    git add '*.ipynb' && git commit -m "re-run with $(git log -1 --format=%s data|cut -d':' -f 1) data."
+    git add '*.ipynb' && git commit -m "re-run with $(stat -c %y data/dati-json/ | cut -f1 -d' ') data."
 fi
 
 if [ ! -z "$STASHED" ]; then
