fd22a55115534751659b7f4aeb7442807e1afcaa
Giuseppe C <AvverbioPronome@users.noreply.github.com>
2020-08-04 15:43:23 +0200

    try using github pages

--- pull.sh
+++ pull.sh
@@ -14,11 +14,24 @@ if [ -n "$(git status --porcelain)" ] || [ "$1" = "--force" ]; then
     git add data && git commit -m "$(date +%s): new commits from pcm-dpc/COVID-19"
     
     docker-compose run jupyter \
-           jupyter nbconvert --inplace --execute './covid/*.ipynb'
-    docker-compose run jupyter \
-           jupyter nbconvert './covid/*.ipynb'
+                   jupyter nbconvert --inplace --execute './covid/*.ipynb'
 
-    git add '*.ipynb' && git commit -m "re-run with $(date --rfc-3339=s --utc -r ./data/dati-json/) data."
+    git add '*.ipynb' \
+        && git commit -m "re-run with $(date --rfc-3339=s --utc -r ./data/dati-json/) data."
+fi
+
+if [ ! -d "html" ] || [ -z "$(ls -A html)" ] || \
+       [ "$(git log -1 --format=%at)" -gt "$(date +%s --utc -r ./html)" ];
+then
+    [ -d "html" ] || mkdir html
+    
+    # for github pages
+    docker-compose run jupyter \
+                   jupyter nbconvert './covid/*.ipynb' --template basic \
+        && mv *.html html/ \
+        && sed -i'' -e '1i ---\n---\n' html/*.html \
+        && git add -f html/*.html \
+        && git commit -m "sync html to commit $(git log -1 --format=%h)"
 fi
 
 if [ -n "$STASHED" ]; then
@@ -26,3 +39,7 @@ if [ -n "$STASHED" ]; then
     STASHED=""
 fi
 
+
+# for local viewing
+# this runs anyway.
+docker-compose run jupyter jupyter nbconvert './covid/*.ipynb'
