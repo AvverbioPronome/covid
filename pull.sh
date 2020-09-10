afb3a604b78a4c70a767577c32eafc3422d78e56
Giuseppe C <AvverbioPronome@users.noreply.github.com>
2020-09-10 09:42:31 +0200

    data is minimized

--- pull.sh
+++ pull.sh
@@ -1,27 +1,38 @@
 #!/bin/sh
 
+_lastmod(){
+    date -u +"%F|%R|%Z" -r "$1"
+}
+
+# make sure relative paths work:
 cd "$(dirname "$0")" || exit 1
+
 
 if [ -n "$(git status --porcelain)" ]; then
     git stash push --quiet
     STASHED="true"
 fi
 
-git pull --quiet
-git submodule update --init --remote
-
+(
+    mkdir -p  ./data/dati-json/ 
+    cd ./data/dati-json/ 
+    wget -N https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-json/dpc-covid19-ita-regioni.json
+) || echo "Unable to download fresh data. Are you online?" && exit 1
+     
 if [ -n "$(git status --porcelain)" ] || [ "$1" = "--force" ]; then
-    git add data && git commit -m "$(date +%s): new commits from pcm-dpc/COVID-19"
+    git add data/* \
+        && git commit -m "$(_lastmod ./data/dati-json/dpc-covid19-ita-regioni.json): new data from pcm-dpc/COVID-19"
     
     docker-compose run jupyter \
                    jupyter nbconvert --inplace --execute './covid/*.ipynb'
 
     git add '*.ipynb' \
-        && git commit -m "re-run with $(date --rfc-3339=s --utc -r ./data/dati-json/) data."
+        && git commit -m "re-run with $(_lastmod ./data/dati-json/dpc-covid19-ita-regioni.json) data."
 fi
 
 if [ ! -d "docs" ] || [ -z "$(ls -A docs)" ] || \
        [ "$(git log -1 --format=%at)" -gt "$(date +%s --utc -r ./docs)" ];
+   # don't use _lastmod here, it's an integer comparison
 then
     [ -d "docs" ] || mkdir docs
     
